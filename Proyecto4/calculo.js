c9a36181629ef90b91ee58ca334c7dca00c4b59d
Manuel1286 <92190334+Manuel1286@users.noreply.github.com>
2021-10-21 00:32:54 -0500

    Cambio 3

--- Proyecto4/calculo.js
+++ Proyecto4/calculo.js
@@ -9,58 +9,136 @@ function ocul_resultado() {
 
 }
 
-///////////////////////////////////CALCULADORA////////////////////////////////////////////////////////////////
+///////////////////////////////////CALCULADORA//////////////////////////////////
+
+
+/////////////////////////////SUMA////////////////////////////////
 
 var boton_calc = document.getElementById("boton_calculadora");
 boton_calc.addEventListener("click", function () {
     ocul_resultado();
     document.getElementById("div_calculadora").style.display = "block";
+});
+
+var suma_calc = document.getElementById("suma");
+suma_calc.addEventListener("click", function () {
+    suma();
 
 });
 
-function calculadora(){
-    var num1=document.getElementById("div_calculadora").value;
-    var res=0;
-    for(var i=1;i<=num1; i++){
-        res=res+i;
-    }
-document.getElementById("div_calculadora").innerText=res;
+function suma() {
+    var numer1 = document.getElementById("numero1").value;
+    var numer2 = document.getElementById("numero2").value;
+    var resultado = parseInt(numer1) + parseInt(numer2);
+    document.getElementById("res").innerHTML = "el resultado es:  " + resultado
 
 }
 
+////////////////////////////////RESTA///////////////////////////////
+
+var resta_calc = document.getElementById("resta");
+resta_calc.addEventListener("click", function () {
+    resta();
+});
 
 
-//////////////////////////////////CONTAR CARACTERES////////////////////////////////////////////////////////////
+function resta(a, b) {
+    var numer1 = document.getElementById("numero1").value;
+    var numer2 = document.getElementById("numero2").value;
+    var resultado = parseInt(numer1) - parseInt(numer2);
+    document.getElementById("res").innerHTML = "el resultado es:  " + resultado
 
+}
+
+////////////////////////////////DIVISION///////////////////////////////
+
+var division_calc = document.getElementById("division");
+division_calc.addEventListener("click", function () {
+    division();
+});
+
+function division(a, b) {
+    var numer1 = document.getElementById("numero1").value;
+    var numer2 = document.getElementById("numero2").value;
+    var resultado = parseInt(numer1) / parseInt(numer2);
+    document.getElementById("res").innerHTML = "el resultado es:  " + resultado;
+
+}
+
+////////////////////////////////MULTIPLICACION///////////////////////////////
+
+
+
+
+
+
+/* function division(a,b){ */
+/*     var numer1=document.getElementById("numero1").value; */
+/*     var numer2=document.getElementById("numero2").value;     */
+/*  */
+/*    if(b!=0){ resultado=parseInt(a)/parseInt(b); */
+/*     return res4; */
+/*  */
+/* }else{ */
+/*     return "no se puede dividir por 0"; */
+/* } */
+/* } */
+/* resultado= division(numer1,numer2); */
+/* alert(resultado); */
+/*  */
+
+
+//////////////////////////////////CONTAR CARACTERES////////////////////////////////////////
+/* boton menu */
 var boton_carac = document.getElementById("boton_caracteres");
 boton_carac.addEventListener("click", function () {
     ocul_resultado();
     document.getElementById("div_contar_caracteres").style.display = "block";
-    document.getElementById("resultado_caracteres").innerText="cantidad de caracteres=  ";
 
 });
 
+/* funcion logica contar caracteres*/
+function caracteres() {
+    var nombre = document.getElementById("input_carateres").value;
+    var caracteres = nombre.length;
+    alert(caracteres);
+    document.getElementById("res_carac").innerHTML = "visualizar carateres es: " +caracteres;
+    
+}
+///////boton gatillo ////////
+var nombre_contar = document.getElementById("btn_carateres");
+nombre_contar.addEventListener("click", function () {
+    caracteres();
+});
 
-
-
-
-
-
-////////////////////////////////MAYUSCULAS & MINUSCULAS///////////////////////////////////////////////////////
-
+///////////////////////////////////MAYUSCULAS & MINUSCULAS/////////////////////////////
+///boton menu/////
 var boton_may = document.getElementById("boton_letras");
 boton_may.addEventListener("click", function () {
     ocul_resultado();
     document.getElementById("div_Mayusculas_&_Minusculas").style.display = "block";
+});
+    
+/* funcion logica convertir a mayu & minus*/
 
+function datos (){
+
+    var texto=document.getElementById("input_mayusculas").value;
+    alert();
+    var texto_mayuscula=texto.toUpperCase();
+    var texto_miniscula=texto.toLowerCase();
+    document.getElementById("resltado_letra").innerHTML="la palabra ingresada es:  "+texto_mayuscula+" "+texto_miniscula;
+}
+
+////////boton garillo ///
+
+var btn_letra=document.getElementById("btn_mayuscula");
+btn_letra.document.addEventListener("click", function(){
+    datos();
 });
 
 
-
-
-
-
-//////////////////////////////////SUMATORIA DE NUMEROS///////////////////////////////////////////////////////
+//////////////////////////////////SUMATORIA DE NUMEROS ///////////////////////////////
 
 var boton_suma = document.getElementById("boton_sumatoria");
 boton_suma.addEventListener("click", function () {
@@ -68,10 +146,3 @@ boton_suma.addEventListener("click", function () {
     document.getElementById("div_sumatoria").style.display = "block";
 
 });
-
-
-
-
-
-
-
